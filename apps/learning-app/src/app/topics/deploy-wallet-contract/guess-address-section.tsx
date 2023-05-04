4a27913670b7a5f3bf1497132446d5b25f3684b2
Inaridiy <inainainariinainari@gmail.com>
2023-05-05 02:07:08 +0900

    refactor: move section

---
+++ apps/learning-app/src/app/topics/deploy-wallet-contract/guess-address-section.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const GuessAddressSection = () => {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold border-b p-2">1. Guess Address</h2>
+      <p className="p-2 text-lg text-muted-foreground">
+        Before deploying the contract, let's guess the address of the contract wallet.
+      </p>
+    </section>
+  );
+};
--- apps/learning-app/src/app/topics/deploy-wallet-contract/page.tsx
+++ apps/learning-app/src/app/topics/deploy-wallet-contract/page.tsx
@@ -1,16 +1,12 @@
 import { TopicHeader } from "@/components/topic/topic-header";
+import { GuessAddressSection } from "./guess-address-section";
 
 export default function Topic() {
   return (
     <>
       <TopicHeader title="Deploy Wallet Contract" />
       <main className="px-2 pt-12 w-full max-w-screen-lg mx-auto">
-        <section>
-          <h2 className="text-3xl font-bold border-b p-2">1. Guess Address</h2>
-          <p className="p-2 text-lg text-muted-foreground">
-            Before deploying the contract, let's guess the address of the contract wallet.
-          </p>
-        </section>
+        <GuessAddressSection />
       </main>
     </>
   );
