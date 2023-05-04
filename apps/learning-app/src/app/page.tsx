24251d00923dd6f1428e11a7a8242c1a876544fe
Inaridiy <inainainariinainari@gmail.com>
2023-05-05 01:03:52 +0900

    feat: courses

--- apps/learning-app/src/app/page.tsx
+++ apps/learning-app/src/app/page.tsx
@@ -1,9 +1,71 @@
-import { ConnectButton } from "@/components/ConnectButton";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const topics = [
+  {
+    id: "deploy-wallet-contract",
+    title: "Step1. Just Deploy Wallet Contract",
+    description: "Deploy a smart contract wallet that supports AccountAbstraction!",
+    steps: [
+      {
+        title: "Guess Address",
+        subtitle: "Guess the address before deploying",
+      },
+      {
+        title: "Deploy Account",
+        subtitle: "Deploy the account contract",
+      },
+    ],
+    startTitle: "Let's GO!!!",
+  },
+] as const;
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ConnectButton />
+    <main className="flex flex-col items-center px-4 py-8 sm:py-24">
+      <h1 className="text-4xl font-bold text-center">
+        Let's Learn <br />
+        Account Abstraction
+      </h1>
+
+      <div className="grid py-12 px-2 sm:px-4 gap-4 sm:grid-cols-2">
+        {topics.map((topic) => (
+          <Card key={topic.id}>
+            <CardHeader>
+              <CardTitle>{topic.title}</CardTitle>
+              <CardDescription>{topic.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              {topic.steps.map((step) => (
+                <div
+                  key={step.title}
+                  className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                >
+                  <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+                  <div className="space-y-1">
+                    <p className="text-sm font-medium leading-none">{step.title}</p>
+                    <p className="text-sm text-muted-foreground">{step.subtitle}</p>
+                  </div>
+                </div>
+              ))}
+            </CardContent>
+            <CardFooter>
+              <Link href="/" className={cn(buttonVariants({}), "w-full")}>
+                {topic.startTitle}
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
---
+++ apps/learning-app/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { ConnectButton } from "./ConnectButton";
+
+export const Header = () => {
+  return (
+    <header className="flex p-4 justify-end w-full mx-auto max-w-screen-lg">
+      <ConnectButton />
+    </header>
+  );
+};
