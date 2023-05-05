8cbd49dfb8c576e0190024ca5fd75dde03c846ef
Inaridiy <inainainariinainari@gmail.com>
2023-05-06 03:07:26 +0900

    fix

--- apps/learning-app/src/app/layout.tsx
+++ apps/learning-app/src/app/layout.tsx
@@ -5,9 +5,34 @@ import "./globals.css";
 import { Providers } from "./providers";
 
 export const metadata = {
+  keywords: ["Ethereum", "Account Abstraction", "AA"],
+  authors: [{ name: "inaridiy", twitter: "https://twitter.com/inaridiy" }],
   title: "Let's Learn Account Abstraction",
-  description:
-    "A site where you can learn AA while working with your hands... What is supposed to be",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.learn-aa.org/",
+    title: "Let's Learn Account Abstraction",
+    description:
+      "A site where you can learn AA while working with your hands... What is supposed to be",
+    siteName: "Learn AA",
+    images: [
+      {
+        url: "https://www.learn-aa.org/ogp.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Learn AA",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Let's Learn Account Abstraction",
+    description:
+      "A site where you can learn AA while working with your hands... What is supposed to be",
+    images: ["https://www.learn-aa.org/ogp.jpg"],
+    creator: "@inaridiy",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
