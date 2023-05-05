df9fe71eee3a9f173795813f9673e3dfbd2e32ad
Inaridiy <inainainariinainari@gmail.com>
2023-05-05 16:38:01 +0900

    feat:  create topic

---
+++ apps/learning-app/src/app/topics/send-tx-with-aa/page.tsx
@@ -0,0 +1,12 @@
+import { TopicHeader } from "@/components/topic/topic-header";
+
+export default function Topic() {
+  return (
+    <>
+      <TopicHeader title="Send Tx with AA" />
+      <main className="px-2 pt-12 w-full max-w-screen-lg mx-auto mb-36">
+        <div className="flex flex-col gap-36"></div>
+      </main>
+    </>
+  );
+}
